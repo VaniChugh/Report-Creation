d325b71568bec93a0eadbb30d4402e37ab3113ee
vallabh chugh <vanichugh18@gmail.com>
2020-09-01 15:08:35 +0530

    Enhancement for the name of the report

--- run_template.py
+++ run_template.py
@@ -1,12 +1,13 @@
 from __future__ import division
-from flask import Flask,render_template
+from flask import Flask
+from datetime import datetime
 from jinja2 import Environment
 test_case_result = {'123': ["Pass", "Expected 1 ", "Actual 1"],
                     '234': ["Fail", "Expected 13 ", "Actual 1"],
                     '345': ["Pass", "Expected 2 ", "Actual 2"],
                     '456': ["Pass", "Expected 12 ", "Actual 12"]}
 
-testsuitename = "FIX Test Suite"
+testsuitename = "FIX_Test_Suite"
 app = Flask(__name__)
 
 @app.template_filter()
@@ -34,19 +35,15 @@ def template_test():
                                                                   testsuite=testsuitename,
                                                                   statusdet=algebrastatus,
                                                                   td=test_case_result)
-            report_filename = "./reports/report.html"
+
+            timestamp = datetime.timestamp(datetime.now())
+            report_filename = "./reports/report_{}_{}.html".format(testsuitename,timestamp)
             with open(report_filename, 'w') as report:
                 report.write(jinja_env)
         return "Success"
     except FileNotFoundError:
         print("File not found")
         return "Failed"
-    # return render_template('template.html',
-    #                        my_string="Wheeeee!",
-    #                        test_case_result=test_case_result,
-    #                        my_list=[0, 1, 2, 3, 4, 5],
-    #                        testsuite="FIX test Suite",
-    #                        current_time=datetime.datetime.now())
 
 if __name__ == '__main__':
     app.run(debug=True)
